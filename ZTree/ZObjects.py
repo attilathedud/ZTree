7f860b2d4a5d81c8ad96e685c32b2bfbce75bcba
Nathan Tucker <thecowgod@thecowgod.local>
2016-02-20 19:25:33 -0800

    Extracting out parsing function and removing unneeded tree_dump

--- ZTree/ZObjects.py
+++ ZTree/ZObjects.py
@@ -1,14 +1,43 @@
 class ZObject:
     def __init__(self):
-        self.ObjectID = 0
-        self.Description = ""
-        self.ParentID = 0
-        self.ChildID = 0
+        self.object_id = 0
+        self.description = ""
+        self.parent_id = 0
+        self.child_id = 0
 
     def display_node(self):
-        print("Object ID: " + str(self.ObjectID) + "\nParent ID: " + str(self.ParentID) + "\nChild ID: " + str(self.ChildID) + "\nDescription: " + self.Description)
+        print("Object ID: " + str(self.object_id) + "\nParent ID: " + str(self.parent_id) + "\nChild ID: " +
+              str(self.child_id) + "\nDescription: " + self.description)
 
 
 class ZGame:
-    def __init__(self):
-        self.ObjectList = []
+    def __init__(self, object_file):
+        self.object_list = []
+        self.object_file = object_file
+
+    def parse_object_file(self):
+        with open(self.object_file, 'r') as file:
+            # Parse header
+            file.readline()
+            line = file.readline()
+            title = line[line.find("is") + 3:]
+            file.readline()
+            file.readline()
+            file.readline()
+            object_total = file.readline()
+
+            cur_object = ZObject()
+
+            # Parse objects
+            for line in file:
+                if line.find(". Attributes") != -1:
+                    cur_object.object_id = int(line[: line.find(". Attributes")].strip())
+                if line.find("Parent object:") != -1:
+                    cur_object.parent_id = int(line[line.find("Parent object:") + 14: line.find("Sibling object:")].strip())
+                    cur_object.child_id = int(line[line.find("Child object:") + 13:])
+                if line.find("Description:") != -1:
+                    cur_object.description = line[line.find("Description:") + 13:].strip()
+                    self.object_list.append(cur_object)
+                    cur_object = ZObject()
+
+        return title, object_total, len(self.object_list)
--- ZTree/ZTree.py
+++ ZTree/ZTree.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from networkx.drawing.nx_agraph import graphviz_layout
 
 def main(argv):
-    object_file = ''
+    object_file = ""
     nodes_to_ignore = []
 
     try:
@@ -28,52 +28,33 @@ def main(argv):
         elif opt in ("-n", "--nodes"):
             nodes_to_ignore = [int(n.strip()) for n in arg.split(',')]
 
-    z_game = zo.ZGame()
+    if not object_file:
+        print('ZTree.py -o <zfile> -n <node1,node2,node3>')
+        sys.exit(2)
 
-    with open('../Dumps/awaken.z5_object_dump', 'r') as file:
+    z_game = zo.ZGame(object_file)
 
-        # Parse header
-        file.readline()
-        line = file.readline()
-        title = line[line.find("is") + 3:]
-        file.readline()
-        file.readline()
-        file.readline()
-        objectTotal = file.readline()
+    title, object_total, object_list_count = z_game.parse_object_file()
 
-        curObject = zo.ZObject()
-
-        # Parse objects
-        for line in file:
-            if line.find(". Attributes") != -1:
-                curObject.ObjectID = int(line[: line.find(". Attributes")].strip())
-            if line.find("Parent object:") != -1:
-                curObject.ParentID = int(line[line.find("Parent object:") + 14: line.find("Sibling object:")].strip())
-                curObject.ChildID = int(line[line.find("Child object:") + 13:])
-            if line.find("Description:") != -1:
-                curObject.Description = line[line.find("Description:") + 13:].strip()
-                z_game.ObjectList.append(curObject)
-                curObject = zo.ZObject()
-
-    print(title + " " + objectTotal)
+    print(title + " " + object_total + " " + str(object_list_count))
     print("Nodes ignored: " + ", ".join(str(i) for i in nodes_to_ignore))
 
     # Set up graph
     G = nx.DiGraph()
 
-    for o in z_game.ObjectList:
-        if o.ParentID in nodes_to_ignore:
-            nodes_to_ignore.append(o.ObjectID)
+    for o in z_game.object_list:
+        if o.parent_id in nodes_to_ignore:
+            nodes_to_ignore.append(o.object_id)
             continue
-        elif o.ObjectID in nodes_to_ignore:
+        elif o.object_id in nodes_to_ignore:
             continue
-        G.add_node(str(o.ObjectID) + "\n" + str(o.Description))
+        G.add_node(str(o.object_id) + "\n" + str(o.description))
 
-    for o in z_game.ObjectList:
-        if o.ParentID != 0 and o.ObjectID not in nodes_to_ignore:
+    for o in z_game.object_list:
+        if o.parent_id != 0 and o.object_id not in nodes_to_ignore:
             # flip this around so arrows are right
-            G.add_edge(str(z_game.ObjectList[o.ParentID - 1].ObjectID) + "\n" + str(z_game.ObjectList[o.ParentID - 1].Description),
-                       str(o.ObjectID) + "\n" + str(o.Description))
+            G.add_edge(str(z_game.object_list[o.parent_id - 1].object_id) + "\n" + str(z_game.object_list[o.parent_id - 1].description),
+                       str(o.object_id) + "\n" + str(o.description))
 
     pos = graphviz_layout(G, prog='dot')
     nx.draw_networkx(G, pos, node_size=1600, node_color='blue', node_alpha=0.3, node_text_size=12)
